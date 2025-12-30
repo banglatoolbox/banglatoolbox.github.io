9044fd7da8aafb070ec7da2b3f883231bd60269e
Md Shahiduzzaman Sifat <138403741+mdsifatgitid@users.noreply.github.com>
2025-12-30 16:20:40 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,21 @@
-
-const CACHE_NAME = 'bangla-toolbox-v10'; // ভার্সন পরিবর্তন করা হয়েছে যাতে নতুন ফাইল আপডেট নেয়
+const CACHE_NAME = 'bangla-toolbox-v8'; // ভার্সন পরিবর্তন করা হয়েছে যাতে নতুন ফাইল আপডেট নেয়
 const urlsToCache = [
-    
-
+    './', 
+    './index.html', // নিশ্চিত করুন আপনার মেইন ফাইলের নাম index.html
+    './manifest.json', // নিশ্চিত করুন ফাইলের নাম manifest.json
+    'https://cdn.tailwindcss.com',
+    'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js',
+    'https://cdn.jsdelivr.net/npm/easyqrcodejs@4.4.13/dist/easy.qrcode.min.js',
+    'https://cdn.jsdelivr.net/npm/jsqr@1.4.0/dist/jsQR.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js',
+    'https://cdn.jsdelivr.net/npm/chart.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.6.1/cropper.min.css',
+    'https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.6.1/cropper.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+    'https://cdn.jsdelivr.net/npm/jsbarcode@3.11.5/dist/JsBarcode.all.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/diff_match_patch/20121119/diff_match_patch.js',
+    'https://cdn.jsdelivr.net/gh/mdsifatgitid/mdsifatgitid.github.io/SUTONNYMJ.TTF',
+    'https://cdn.jsdelivr.net/gh/mdsifatgitid/mdsifatgitid.github.io/Bornomala-Regular.ttf'
 ];
 
 // ১. ইনস্টল ইভেন্ট
